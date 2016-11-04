afbba6a2882d7f0ae69b2fab50d635c384684c55
kyn27500 <275004595@qq.com>
2016-11-04 18:03:27 +0800

    123

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  
+  // 工具ID号
+  var id = req.query.id;
+  
 });
 
 module.exports = router;
