009ccd05c81cd261f88c9ce4bdaafa68f232e8c0
kyn27500 <275004595@qq.com>
2016-11-09 15:01:04 +0800

    test

--- routes/excel/ExcelToLua.py
+++ routes/excel/ExcelToLua.py
@@ -2,13 +2,6 @@
 #Author: kyn
 #Date: 2015-10-10
 #Purpose: 解析excel数据表到lua
-
-# excel 路径
-# xls_path = "/Users/wangmeili/Documents/ExcelTolua"
-xls_path = "/Users/wangmeili/Documents/workspace/nodejs/GameTools/test"
-# lua文件导出的路径
-lua_path = "/Users/wangmeili/Documents/workspace/nodejs/GameTools/test"
-
 import os
 import sys
 import string
@@ -16,12 +9,20 @@ import xlrd
 reload(sys)
 sys.setdefaultencoding('utf8') 
 
-tab = "	"
-errorstr = "data error"
-errorFlag = 0
+# excel 路径
+# xls_path = "/Users/wangmeili/Documents/ExcelTolua"
+xls_path = "/Users/koba/Documents/workspace/NodeJs/gametools/test"
+# lua文件导出的路径
+lua_path = "/Users/koba/Documents/workspace/NodeJs/gametools/test/out"
+# 
+testXlsx = "/Users/koba/Documents/workspace/NodeJs/gametools/test/004.xlsx"
+
+
+_errorDes = "data error"
+_isError = False
 
 # 按类型解析数据
-dataType = {"number" : 1 ,"string" : 2 ,"array1" : 3}	#定义数据类别 array1：一维数组
+dataType = ["number","string","array1"]	#定义数据类别 array1：一维数组
 
 # 查找所有 xls
 def findAllFile(dirPath,callback):
@@ -48,9 +49,7 @@ def split(pData):
 	return ret
 
 # 检查表大小
-def checkTableSize():
-	data = xlrd.open_workbook("/Users/wangmeili/Documents/workspace/nodejs/GameTools/test/004.xlsx")
-	pTabData = data.sheet_by_index(0)		#通过索引顺序获取
+def checkTableSize(pTabData):
 	# 获取表格默认大小
 	nrows = pTabData.nrows
 	ncols = pTabData.ncols
@@ -75,131 +74,146 @@ def checkTableSize():
 				# 	return "ID 错误"
 	return retRow,retCol
 
-# 获取表格数据
-def getDataByExcel(pRow,pCol):
-	data = xlrd.open_workbook("/Users/wangmeili/Documents/workspace/nodejs/GameTools/test/004.xlsx")
-	pTabData = data.sheet_by_index(0)		#通过索引顺序获取
+# 获取表格表头数据
+def getTabTitleByExcel(pTabData,pCol):
+	tabKey = []
+	tabKeyType=[]
+	for col in range(pCol):
+		keyCell = pTabData.cell(2,col+1)
+		typeCell = pTabData.cell(1,col+1)
+		if keyCell.ctype == 0:
+			tabKey.append("Error")
+		else:
+			tabKey.append(keyCell.value)
 
-	ret = {}
+		if typeCell.ctype == 0:
+			tabKeyType.append("Error")
+		else:
+			tabKeyType.append(split(typeCell.value))
+
+	return tabKey,tabKeyType
+
+# 获取表格数据
+def getDataByExcel(pTabData,pRow,pCol):
+	ret = []
 	for row in range(pRow):
-		ret[row]={}
+		coltab=[]
 		for col in range(pCol):
-			ret[row][col] = pTabData.cell(row+3,col+1).value
+			coltab.append(pTabData.cell(row+3,col+1).value)
+		ret.append(coltab)
 	return ret
 
-def parseDataByType( dType , cell ):
+
+# 文件介绍
+_fileDes='''-- Filename: %s.lua
+-- Author: auto-created by kong`s ParseExcel(to lua) tool.
+-- methods: X.keys = {}, X.getDataById(id), X.getArrDataByField(fieldName, fieldValue)
+-- Function: no description.\n
+'''
+_fileKeys = "local keys = {\n\t\"%s\"\n}\n\n"
+_fileData = "local data = {\n%s}\n\n"
+_fileDataItem = "\tid%s={%s},\n"
+_fileReturn = "%s = DB_Template.new (keys, data)\nreturn %s"
+
+# 构造输出lua
+def getLuaText(pTabTitle,pData,fileName):
+	# 页面内容分四部分，介绍，keys,data,retrun template
+
+	# 文件介绍
+	luaFileName = "DB_%s" % (fileName.capitalize())
+	fileStr1 = _fileDes%(luaFileName)
+	# 文件keys
+	fileStr2 = _fileKeys%('\",\"'.join(pTabTitle[0]))
+
+	# 文件数据内容
+	filedata = parseData(pTabTitle[1],pData)
+	fileStr3 = _fileData%filedata
+	# print(list(pTabTitle))
+
+	# 文件返回
+	fileStr4 = _fileReturn%(luaFileName,luaFileName)
+	fileStr = fileStr1+fileStr2+fileStr3+fileStr4
+
+	return fileStr
+
+# lua文件数据内容整理
+def parseData(pKeyType,pData):
+
+	ret = ""
+	for row in range(len(pData)):
+		item=[str(row+1)]
+
+		for col in range(len(pData[1])):
+			item.append(parseDataByType(pKeyType[col],pData[row][col]))
+		fileItem = _fileDataItem%(row+1,", ".join(item))
+		ret = ret+fileItem
+	return ret
+
+# 根据不同类型，返回相应数据（以后添加类型直接改这里）
+def parseDataByType(pkeyType,pData):
 	
-	ctype = split(dType)
-	cType = cell.ctype 		# 类型 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
-	cValue = cell.value
+	isMustHasData = pkeyType[1] == "yes"
+	isHasData = pData != ''
 
-	if cmp(str(dType[1]),"yes") == 0:
-		if cType == 0:
-			errorFlag = 1
-			return errorstr
+	# 检测是否必填数据，但没有数据（漏写数据）
+	if isMustHasData and (not isHasData):
+		return "errordata"
+	elif(not isHasData):
+		return "nil"
 	else:
-		if cType == 0:
-			return "nil"
 
-	if dataType[dType[0]] == 1:
-		if cType == 1:
-			return int(cValue.strip())
-		elif cType == 2:
-			return int(cValue)
-	elif dataType[dType[0]] == 2:
-		return "\"%s\"" % (cValue)
-	elif dataType[dType[0]] == 3:
-		return "{%s}" % (cValue)
+		# 数字
+		if pkeyType[0] == dataType[0]:
+			if int(pData) == pData:
+				return str(int(pData))
+			else:
+				return str(pData)
+		# 字符串
+		elif pkeyType[0] == dataType[1]:
+			return "\"%s\"" % pData
+		# 数组
+		elif pkeyType[0] == dataType[2]:
+			tmp=[]
+			for val in pData:
+				tmp.append(str(val))
+
+			return "{%s}" % ", ".join(tmp)
+
 
 # 写文件
-def writeToLua(filePath,fileKeys,fileContent):
-	# filePath = filePath.replace("\\\\","/")
+def writeToLua(filePath,fileName,fileData):
+
 
 	fDirPath = os.path.dirname(filePath)
 	fDirPath = fDirPath.replace(xls_path,lua_path)
 	if not os.path.exists(fDirPath):
 		os.mkdir(fDirPath)
 
-	fileName = "DB_%s" % (filePath[filePath.rindex("\\")+1:filePath.find(".")].capitalize())
-
-	fileStart ='''
--- Filename: %s.lua
--- Author: auto-created by kong`s ParseExcel(to lua) tool.
--- methods: X.keys = {}, X.getDataById(id), X.getArrDataByField(fieldName, fieldValue)
--- Function: no description.\n
-'''
-	fileStart = fileStart % (fileName)
-
-	fileEnd = '''
-%s = DB_Template.new (keys, data)
-return %s
-'''
-	fileEnd = fileEnd % (fileName,fileName)
-
-	fileStr = fileStart + fileKeys + fileContent + fileEnd
-
 	filePath = os.path.join(fDirPath,"%s.lua" % (fileName))
 	f = open(filePath,"w")
 	f.write(fileStr)
 	f.close()
 
-# 读取 excel 表
-def parseExcel(filePath):
-	fileContent = "local data = {\n"
-	fileKeys = "local keys = {\n" + tab
-	data = xlrd.open_workbook(filePath)
-	# table = data.sheets()[0]          #通过索引顺序获取
-	table = data.sheet_by_index(0)		#通过索引顺序获取
 
-	nrows = table.nrows
-
-	ncols = table.ncols
-
-	# get file keys 
-	for col in range(ncols):
-		if table.cell(1,col).ctype == 0:
-				continue
-		fileKeys = "%s\"%s\"," % (fileKeys,str(table.cell(2,col).value).strip())
-	fileKeys = "%s\n}\n\n" % (fileKeys[0:-1])
-
-	# get file content
-	for row in range(3,nrows):
-		rowStr = ""
-		for col in range(ncols):
-			# 该列类型为空，直接返回
-			if table.cell(1,col).ctype == 0:
-				continue
-			# 类型 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
-			cell = table.cell(row,col)
-			dataType = table.cell(1,col).value
-			if col == 0:
-				dataType = "number_yes"
-			curValue = parseDataByType(dataType,cell)
-
-			if col == 0:
-				rowStr = "id"+str(curValue)+" = {"
-			
-			rowStr = "%s %s," % (rowStr,curValue)
-
-		if errorFlag == 1:
-			break
-		rowStr = "%s%s},\n" % (tab,rowStr[0:-1])
-		fileContent = fileContent + rowStr
-	fileContent = "%s\n}\n" % (fileContent[0:-2])
-
-	writeToLua(filePath,fileKeys,fileContent)
-    
 if __name__ == '__main__':
 
 	if not os.path.exists(lua_path):
 		os.mkdir(lua_path)
 
+
+	data = xlrd.open_workbook(testXlsx)
+	pTabData = data.sheet_by_index(0)		#通过索引顺序获取
+
 	# findAllFile(xls_path,parseExcel)
+	size = checkTableSize(pTabData)
+	tabTitle = getTabTitleByExcel(pTabData,size[1])
+	data1 = getDataByExcel(pTabData,size[0],size[1])
+	# for i in range(pTabData.nrows):
+	# 	print(pTabData.row_values(i))
+	# print(size)
+	# print(tabTitle)
+	print(data1)
 
-	# print(split("number_yes"))
-	size = checkTableSize()
-	data = getDataByExcel(size[0],size[1])
-	print(data)
-
-
-
+	fileStr = getLuaText(tabTitle,data1,"testExcel")
+	writeToLua(lua_path,"testExcel",fileStr)
+	print(fileStr)
